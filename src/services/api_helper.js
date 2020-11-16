364d9ad1146cfe92af83c6e5a39392ef431b45bf
Brady Dawley <dawleybradym@johndeere.com>
2020-11-16 12:06:32 -0600

    transferred back to local

--- src/services/api_helper.js
+++ src/services/api_helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://cbb-model.herokuapp.com" 
+  baseURL: "http://localhost:3001"  //"https://cbb-model.herokuapp.com" 
 })
 
 // ============ AUTH =============

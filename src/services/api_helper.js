948cfa54070232ee66048ab7a9e0255e874def66
Brady Dawley <dawleybradym@johndeere.com>
2020-11-16 09:26:54 -0600

    updated to deployed URLs

--- src/services/api_helper.js
+++ src/services/api_helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3001" // "https://heroku.app.blah.com"
+  baseURL: "https://cbb-model.herokuapp.com" 
 })
 
 // ============ AUTH =============
